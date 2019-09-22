7232a7f0aa9faaffa750d8543b6d734cff3e1aee
aabcceccbba <kaiyuelu@gmail.com>
2019-09-22 16:33:56 -0500

    Initial commit

---
+++ PiratePassage/src/PiratePassage.java
@@ -0,0 +1,25 @@
+
+public class PiratePassage {
+	public void input() {
+		// receive input
+		// pirates original locations
+		// ship original location
+		// destination location
+		// unavailable locations
+	}
+	
+	public void precal() {
+		// pre-calculate the mode steps for unavailable locations
+	}
+	
+	public void path() {
+		// each step -> possible next steps
+	}
+	
+	public static void main(String[] args) {
+		PiratePassage pp = new PiratePassage();
+		pp.input();
+		pp.precal();
+		pp.path();
+	}
+}
