486cd9c63305d22485482c0c12d30c934af1c752
aabcceccbba <kaiyuelu@gmail.com>
2019-09-22 16:56:06 -0500

    added Location class

---
+++ PiratePassage/src/Location.java
@@ -0,0 +1,13 @@
+
+public class Location {
+	int x;
+	int y;
+	// dir should be ("up", "down", "right", "left")
+	String dir;
+	
+	public Location(int x, int y, String dir) {
+		this.x = x;
+		this.y = y;
+		this.dir = dir;
+	}
+}
--- PiratePassage/src/PiratePassage.java
+++ PiratePassage/src/PiratePassage.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 
 public class PiratePassage {
 	public void input() {
 		// receive input
+		System.out.println("Please enter the number of the pirates: ");
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		Location[] ploc = new Location[n];
 		// pirates original locations
+		for(int i = 0; i < n; i++) {
+			System.out.println("Please input the location(x, y, dir) of priate " + i + ": ");
+		}
 		// ship original location
 		// destination location
 		// unavailable locations
