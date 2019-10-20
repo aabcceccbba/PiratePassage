ed362d7ec93596174ea406dd2a8811deb1c710fe
aabcceccbba <kaiyuelu@gmail.com>
2019-10-19 21:27:38 -0500

    add Ship class and ask for ship location

--- PiratePassage/src/PiratePassage.java
+++ PiratePassage/src/PiratePassage.java
@@ -1,17 +1,52 @@
 import java.util.Scanner;
 
 public class PiratePassage {
+	
+	public Location inputLocation(Scanner scan, int i) {
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		String dir = scan.next();
+		Location loc = new Location(x, y, dir);
+		return loc;
+	}
+	
 	public void input() {
 		// receive input
 		System.out.println("Please enter the number of the pirates: ");
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		Location[] ploc = new Location[n];
+		Ship[] pirates = new Ship[n];
+		
 		// pirates original locations
 		for(int i = 0; i < n; i++) {
-			System.out.println("Please input the location(x, y, dir) of priate " + i + ": ");
+			System.out.println("Please input the color of the Pirate: ");
+			String color = scan.next();
+			System.out.println("Left Bottom is (0,0), dir can be either up, down, left, right");
+			System.out.println("Please input the start location(x, y, dir) of priate " + i + ": ");
+			int x = scan.nextInt();
+			int y = scan.nextInt();
+			String dir = scan.next();
+			Location start = new Location(x, y, dir);
+			System.out.println("Please input the diagonal location(x, y, dir) of pirate" + i + ": ");
+			int dx = scan.nextInt();
+			int dy = scan.nextInt();
+			String ddir = scan.next();
+			Location diagonal = new Location(dx, dy, ddir);
+			
+			pirates[i] = new Ship(color, start, diagonal); 		
 		}
+		
 		// ship original location
+		System.out.println("Please input the start location(x, y, dir) of your ship: ");
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		String dir = scan.next();
+		Location start = new Location(x, y, dir);
+		System.out.println("Please input the diagonal location(x, y, dir) of your ship: ");
+		int dx = scan.nextInt();
+		int dy = scan.nextInt();
+		String ddir = scan.next();
+		Location diagonal = new Location(dx, dy, ddir);
 		// destination location
 		// unavailable locations
 	}
---
+++ PiratePassage/src/Ship.java
@@ -0,0 +1,13 @@
+
+public class Ship {
+	
+	String color;
+	Location start;
+	Location diagonal;
+	
+	public Ship(String color, Location start, Location diagonal) {
+		this.color = color;
+		this.start = start;
+		this.diagonal = diagonal;
+	}
+}
